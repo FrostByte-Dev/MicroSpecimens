4b9da18712ebe647f9abd5392b6459d45ca1fcad
Jacques Levasseur <jacques.levasseur@trilliumcollege.ca>
2017-07-11 16:06:01 -0400

    Added a loop to the entire program that allows you to enter exit to quit the program.

--- Microspecimens2.py
+++ Microspecimens2.py
@@ -23,120 +23,123 @@ term_list = ["selenite", "hektoen", "mac", "smac", "macconkey", "sorbitol", "cam
 specimen_list = [[stool], [sputum], [ear], [throat], [urine], [mro], [wound], [soft_tissue], [bite_wound], [deep_wound], [below_waist], [csf], [sterile], [eye], [vaginal], [groupbscreen], [cervical], [urethral]]
 
 #Add a loop that will keep the program going as long as the user does not enter "exit"
-print("Welcome to the micro plate-o-bot 4000 main menu. What would you like to do?")
-print("1-Specimen Search:Enter 1 to view a specimen type and see the plates required as well as further information")
-print("2-Element Back Search:Enter 2 to search up a plate or characteristic")
-print("3-Term List:Enter 3 to see a list of all the terms contained. I recommend this before using the element back search.")
-menu_option = input("Enter your decision here: ")
-menu_option2 = int(menu_option)
+menu_option = 0
 menucounter = 0 #Loop for going back to menu if input is not available
-if menu_option2 == 1:
-    while menucounter < 1:
-        print("Welcome. Enter the type of specimen you would like to search up. Please enter the type as designated in the following list")
-        print("stool,sputum,ear,throat,urine,mro,wound,soft_tissue,bite_wound,deep_wound,below_waist,csf, sterile, eye. vaginal, groupbscreen,cervical,urethral ")
-        specimen = input("Enter the type here: ")
-        specimen = specimen.lower()
-        if specimen == "stool":
-            print(stool)
-            menucounter = 2
-        elif specimen == "sputum":
-            print(sputum)
-            menucounter = 2
-        elif specimen == "ear":
-            print(ear)
-            menucounter = 2
-        elif specimen == "throat":
-            print(throat)
-            menucounter = 2
-        elif specimen == "urine":
-            print(urine)
-            menucounter = 2
-        elif specimen == "mro":
-            print(mro)
-            menucounter = 2
-        elif specimen == "wound":
-            print(wound)
-            menucounter = 2
-        elif specimen == "soft_tissue":
-            print(soft_tissue)
-        elif specimen == "bite_wound":
-            print(bite_wound)
-            menucounter = 2
-        elif specimen == "deep_wound":
-            print(deep_wound)
-            menucounter = 2
-        elif specimen == "below_waist":
-            print(below_waist)
-            menucounter = 2
-        elif specimen == "csf":
-            print(csf)
-            menucounter = 2
-        elif specimen == "sterile":
-            print(sterile)
-            menucounter = 2
-        elif specimen == "eye":
-            print(eye)
-            menucounter = 2
-        elif specimen == "vaginal":
-            print(vaginal)
-            menucounter = 2
-        elif specimen == "groupbscreen":
-            print(groupbscreen)
-            menucounter = 2
-        elif specimen == "cervical":
-            print(cervical)
-            menucounter = 2
-        elif specimen == "urethral":
-            print(urethral)
-            menucounter = 2
-        else:
-            print("That is not an acceptable specimen. Please try again using the provided list.")
-elif menu_option2 == 2:
-    print("Enter the type of plate or elements you would like to look up, note that the database only contains certain variations which are generally short forms and acronyms.")
-    plate = input("Enter the desired plate or element here: ")
-    plate = plate.lower()
-    if plate in stool:
-        print("Element contained in stool processing")
-    if plate in sputum:
-        print("Element contained in sputum processing")
-    if plate in ear:
-        print("Element contained in ear processing")
-    if plate in throat:
-        print("Element contained in throat processing")
-    if plate in urine:
-        print("Element contained in urine processing")
-    if plate in mro:
-        print("Element contained in stool processing")
-    if plate in wound:
-        print("Element contained in normal wound processing")
-    if plate in soft_tissue:
-        print("Element contained in soft tissue processing")
-    if plate in bite_wound:
-        print("Element contained in bite wound processing")
-    if plate in deep_wound:
-        print("Element contained in deep wound processing")
-    if plate in below_waist:
-        print("Element contained in below the waist wound processing")
-    if plate in csf:
-        print("Element contained in csf processing")
-    if plate in sterile:
-        print("Element contained in sterile site processing")
-    if plate in eye:
-        print("Element contained in eye/conjunctivitis processing")
-    if plate in vaginal:
-        print("Element contained in vaginal processing")
-    if plate in groupbscreen:
-        print("Element contained in Group B Screening")
-    if plate in cervical:
-        print("Element contained in cervical processing")
-    if plate in urethral:
-        print("Element contained in urethral processing")
-    if plate not in term_list:
-        print("Choice is not in list of terms. Please consult the following list")
-        term_listdup = set(term_list)
-        print(sorted(term_listdup))
-elif menu_option2 == 3:
-    term_listdup = set(term_list) #When making the term list I was too lazy to check for duplicates so this removes them.....
-    print(sorted(term_listdup)) #alphabetical order for ease of use
-    #Need to work out way to separate lists and sort individual terms alphabetically ratherh than sublists
+while menu_option != "exit":
+    print("Welcome to the micro plate-o-bot 4000 main menu. What would you like to do?")
+    print("1-Specimen Search:Enter 1 to view a specimen type and see the plates required as well as further information")
+    print("2-Element Back Search:Enter 2 to search up a plate or characteristic")
+    print("3-Term List:Enter 3 to see a list of all the terms contained. I recommend this before using the element back search.")
+    print("Enter exit once you are done to close the program.")
+    menu_option = input("Enter your decision here: ")
+
+    if menu_option == "1":
+        while menucounter < 1:
+            print("Welcome. Enter the type of specimen you would like to search up. Please enter the type as designated in the following list")
+            print("stool,sputum,ear,throat,urine,mro,wound,soft_tissue,bite_wound,deep_wound,below_waist,csf, sterile, eye. vaginal, groupbscreen,cervical,urethral ")
+            specimen = input("Enter the type here: ")
+            specimen = specimen.lower()
+            if specimen == "stool":
+                print(stool)
+                menucounter = 2
+            elif specimen == "sputum":
+                print(sputum)
+                menucounter = 2
+            elif specimen == "ear":
+                print(ear)
+                menucounter = 2
+            elif specimen == "throat":
+                print(throat)
+                menucounter = 2
+            elif specimen == "urine":
+                print(urine)
+                menucounter = 2
+            elif specimen == "mro":
+                print(mro)
+                menucounter = 2
+            elif specimen == "wound":
+                print(wound)
+                menucounter = 2
+            elif specimen == "soft_tissue":
+                print(soft_tissue)
+            elif specimen == "bite_wound":
+                print(bite_wound)
+                menucounter = 2
+            elif specimen == "deep_wound":
+                print(deep_wound)
+                menucounter = 2
+            elif specimen == "below_waist":
+                print(below_waist)
+                menucounter = 2
+            elif specimen == "csf":
+                print(csf)
+                menucounter = 2
+            elif specimen == "sterile":
+                print(sterile)
+                menucounter = 2
+            elif specimen == "eye":
+                print(eye)
+                menucounter = 2
+            elif specimen == "vaginal":
+                print(vaginal)
+                menucounter = 2
+            elif specimen == "groupbscreen":
+                print(groupbscreen)
+                menucounter = 2
+            elif specimen == "cervical":
+                print(cervical)
+                menucounter = 2
+            elif specimen == "urethral":
+                print(urethral)
+                menucounter = 2
+            else:
+                print("That is not an acceptable specimen. Please try again using the provided list.")
+    elif menu_option == "2":
+        print("Enter the type of plate or elements you would like to look up, note that the database only contains certain variations which are generally short forms and acronyms.")
+        plate = input("Enter the desired plate or element here: ")
+        plate = plate.lower()
+        if plate in stool:
+            print("Element contained in stool processing")
+        if plate in sputum:
+            print("Element contained in sputum processing")
+        if plate in ear:
+            print("Element contained in ear processing")
+        if plate in throat:
+            print("Element contained in throat processing")
+        if plate in urine:
+            print("Element contained in urine processing")
+        if plate in mro:
+            print("Element contained in stool processing")
+        if plate in wound:
+            print("Element contained in normal wound processing")
+        if plate in soft_tissue:
+            print("Element contained in soft tissue processing")
+        if plate in bite_wound:
+            print("Element contained in bite wound processing")
+        if plate in deep_wound:
+            print("Element contained in deep wound processing")
+        if plate in below_waist:
+            print("Element contained in below the waist wound processing")
+        if plate in csf:
+            print("Element contained in csf processing")
+        if plate in sterile:
+            print("Element contained in sterile site processing")
+        if plate in eye:
+            print("Element contained in eye/conjunctivitis processing")
+        if plate in vaginal:
+            print("Element contained in vaginal processing")
+        if plate in groupbscreen:
+            print("Element contained in Group B Screening")
+        if plate in cervical:
+            print("Element contained in cervical processing")
+        if plate in urethral:
+            print("Element contained in urethral processing")
+        if plate not in term_list:
+            print("Choice is not in list of terms. Please consult the following list")
+            term_listdup = set(term_list)
+            print(sorted(term_listdup))
+    elif menu_option == "3":
+        term_listdup = set(term_list) #When making the term list I was too lazy to check for duplicates so this removes them.....
+        print(sorted(term_listdup)) #alphabetical order for ease of use
+        #Need to work out way to separate lists and sort individual terms alphabetically ratherh than sublists
     
